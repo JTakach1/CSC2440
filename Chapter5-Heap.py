30cfe2ad23379b6f609142c5f9bf6d6a164790d7
JTakach1 <156825794+JTakach1@users.noreply.github.com>
2024-11-28 20:06:01 -0500

    Create Chapter5-Heap.py

---
+++ Chapter5-Heap.py
@@ -0,0 +1,28 @@
+import heapq
+def input_tree_values():
+    arr = []
+    while True:
+        value = input("Enter tree values one at a time. Type 'done' when you're finished: ")
+        if value.lower() == 'done':
+            break
+        arr.append(int(value))
+        return arr
+
+
+def heap_sort(arr):
+    heapq.heapify(arr)
+    sorted_arr = []
+    while arr:
+        smallest = heapq.heappop(arr)
+        sorted_arr.insert(0, smallest)
+
+    return sorted_arr
+
+def print_sorted_values(arr):
+    print("Sorted values (in decreasing order):")
+    print(arr)
+
+
+arr = input_tree_values()
+sorted_arr = heap_sort(arr)
+print_sorted_values(sorted_arr)
